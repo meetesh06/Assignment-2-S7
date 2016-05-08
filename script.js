bf842e94f62a50c6b53238c315efe89dde68b6ef
meetesh06 <meeteshmehta4@gmail.com>
2016-05-08 23:27:36 +0530

    Improve code

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 var table = document.getElementById("tab_1");
-var tr1 = table.insertRow(0);
-var tr2 = table.insertRow(1);
-var tr3 = table.insertRow(2);
-var tr4 = table.insertRow(3);
-var tr5 = table.insertRow(4);
+for (var i = 0; i < 10; i++) { //insert 5 rows and create cells, then add the content
+	table.insertRow(i).insertCell(0).innerHTML = "row " + (i + 1);
+}
 var rows = table.rows;
-
-var td1 = tr1.insertCell(0);
-td1.innerHTML = "row 1"
-var td2 = tr2.insertCell(0);
-td2.innerHTML = "row 2"
-var td3 = tr3.insertCell(0);
-td3.innerHTML = "row 3"
-var td4 = tr4.insertCell(0);
-td4.innerHTML = "row 4"
-var td5 = tr5.insertCell(0);
-td5.innerHTML = "row 5"
-
-function rows_alter(a) {
-	for (var i = 0; i < table.rows.length; i++) {
-		if (i % 2 == 0) {
+var counter = 0;
+function change_display (a) {
+	for (var i = 0; i < rows.length; i++) {
+		if (i % 2 === 0) {
 			rows[i].style.display = a;
 		}
 	}
 }
-
-var button_odd = document.getElementById('odd');
-button_odd.addEventListener("click", function() {
-	rows_alter("none");
-});
-var button_even = document.getElementById('even');
-button_even.addEventListener("click", function() {
-	rows_alter("table-row");
-});
+function hide_rows() {
+	button[0].innerHTML = "SHOW ODD ROWS";
+	button[0].style.background = "red";
+	change_display ("none");
+}
+function show_rows() {
+	button[0].innerHTML = "HIDE ODD ROWS";
+	button[0].style.background = "#76C4E7";
+	change_display ("table-row");
+	
+}
+function rows_alter() {
+	if (counter % 2 === 0) {
+		hide_rows();
+	} else {
+		show_rows();
+	}
+	counter++;
+}
+var button = document.getElementsByClassName('button');
+button[0].addEventListener("click", rows_alter);
